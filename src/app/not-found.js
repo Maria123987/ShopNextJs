52c698a069892a08e6ce5f3b0b73d723cfad478f
Reza Paidar <reza.paidar@outlook.com>
2026-06-18 12:20:37 +0330

    Create not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+            <a href="/">Return Home</a>
+        </div>
+    )
+}
+
+export default NotFound
